10ac7a94d4685b3d948ced66b4f17992a29123d1
Yael2924 <saulyaeld@gmail.com>
2026-01-23 10:52:14 -0600

    parte de breeze

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PaginasController;
-/*Route::get('/', function () {
+
+Route::get('/', function () {
     return view('welcome');
-});*/
+});
 
-Route::get('/', [PaginasController::class, 'inicio'])->name('inicio');
-Route::get('/nosotros', [PaginasController::class, 'nosotros'])->name('nosotros');
-Route::get('/servicios', [PaginasController::class, 'servicios'])->name('servicios');
-Route::get('/contacto', [PaginasController::class, 'contacto'])->name('contacto');
-Route::get('/ayuda', [PaginasController::class, 'ayuda'])->name('ayuda');
-Route::get('/buzon', [PaginasController::class, 'buzon'])->name('buzon');
-Route::get('/mapa', [PaginasController::class, 'mapa'])->name('mapa');
-Route::get('/chat', [PaginasController::class, 'chat'])->name('chat');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/buscar', [PaginasController::class, 'buscar'])->name('buscar');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
+require __DIR__.'/auth.php';
